221ada999322d430f4f18824ac4a1fe2ad9ad7b5
fatasoft consulting <fatahouahamadi88@gmail.com>
2025-09-16 10:19:49 +0100

    Add enrichment data userproifle

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Pour dev
-export const BASE_URL_API = "http://localhost:4001";
+// export const BASE_URL_API = "http://localhost:4001";
 
 // Pour prod (à décommenter en déploiement)
-// export const BASE_URL_API = "https://api.zpdigital.fr";
+export const BASE_URL_API = "https://api.zpdigital.fr";
